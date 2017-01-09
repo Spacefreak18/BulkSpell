0bdd58c3c77290301001638112507e4506324043
Paul Jones <pjones@corp.ipipenet.com>
2017-01-09 11:42:34 -0500

    Adding folder dialog to UI. Making app dynamic.

--- src/MainForm.Designer.cs
+++ src/MainForm.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -37,28 +40,50 @@
             this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(1, 39);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 57);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.Size = new System.Drawing.Size(1056, 901);
+            this.dataGridView1.Size = new System.Drawing.Size(1106, 892);
             this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(22, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Folder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(115, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(425, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "None";
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1057, 947);
+            this.ClientSize = new System.Drawing.Size(1130, 961);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "MainForm";
-            this.Text = "MainForm";
+            this.Text = "Bulkspell";
             this.Load += new System.EventHandler(this.MainForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- src/MainForm.cs
+++ src/MainForm.cs
@@ -19,13 +19,37 @@ namespace BulkSpell
 
         private void MainForm_Load(object sender, EventArgs e)
         {
+
             DataTable dt = new DataTable();
             dt.Columns.AddRange(new DataColumn[3] { new DataColumn("Id"), new DataColumn("Name"), new DataColumn("Errors") });
 
             System.IO.FileInfo[] filesList = null;
 
             System.IO.DirectoryInfo directory;
-            directory = new System.IO.DirectoryInfo(@"C:\Paul\pdfs\");
+            directory = new System.IO.DirectoryInfo(textBox1.Text);
+            filesList = directory.GetFiles();
+
+            int i = 1;
+            foreach (System.IO.FileInfo file in filesList)
+            {
+                dt.Rows.Add(i, file.Name, "errors detected");
+            }
+            dataGridView1.DataSource = dt;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+
+            textBox1.Text = folderBrowserDialog1.SelectedPath;
+
+            DataTable dt = new DataTable();
+            dt.Columns.AddRange(new DataColumn[3] { new DataColumn("Id"), new DataColumn("Name"), new DataColumn("Errors") });
+
+            System.IO.FileInfo[] filesList = null;
+
+            System.IO.DirectoryInfo directory;
+            directory = new System.IO.DirectoryInfo(textBox1.Text);
             filesList = directory.GetFiles();
 
             int i = 1;
