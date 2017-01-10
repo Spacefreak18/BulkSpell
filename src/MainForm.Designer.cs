83b3770eda5ea2641da1860a8537ab9a3b6dd03e
Paul Jones <pjones@corp.ipipenet.com>
2017-01-10 10:56:29 -0500

    Added count for total unique misspellings and button to show unique list.

--- src/MainForm.Designer.cs
+++ src/MainForm.Designer.cs
@@ -28,10 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -43,7 +47,7 @@
             this.dataGridView1.Location = new System.Drawing.Point(12, 57);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.Size = new System.Drawing.Size(1106, 892);
+            this.dataGridView1.Size = new System.Drawing.Size(1106, 758);
             this.dataGridView1.TabIndex = 0;
             // 
             // button1
@@ -64,12 +68,45 @@
             this.textBox1.TabIndex = 2;
             this.textBox1.Text = "None";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 830);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Total Unique Misspellings:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(158, 830);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "0";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(242, 825);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "ReverseList";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // MainForm
             // 
-            this.ClientSize = new System.Drawing.Size(1130, 961);
+            this.ClientSize = new System.Drawing.Size(1130, 855);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainForm";
             this.Text = "Bulkspell";
             this.Load += new System.EventHandler(this.MainForm_Load);
@@ -85,5 +122,8 @@
         private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
     }
 }
--- src/MainForm.cs
+++ src/MainForm.cs
@@ -7,11 +7,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NetSpell.SpellChecker;
 
 namespace BulkSpell
 {
     public partial class MainForm : Form
     {
+        private Spelling s;
+        private int mispellings;
+        private Dictionary<string, List<string>> WrongWords = new Dictionary<string,List<string>>();
+        private string currentfilename;
+
         public MainForm()
         {
             InitializeComponent();
@@ -20,25 +26,18 @@ namespace BulkSpell
         private void MainForm_Load(object sender, EventArgs e)
         {
 
-            DataTable dt = new DataTable();
-            dt.Columns.AddRange(new DataColumn[3] { new DataColumn("Id"), new DataColumn("Name"), new DataColumn("Errors") });
-
-            System.IO.FileInfo[] filesList = null;
-
-            System.IO.DirectoryInfo directory;
-            directory = new System.IO.DirectoryInfo(textBox1.Text);
-            filesList = directory.GetFiles();
-
-            int i = 1;
-            foreach (System.IO.FileInfo file in filesList)
-            {
-                dt.Rows.Add(i, file.Name, "errors detected");
-            }
-            dataGridView1.DataSource = dt;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            this.components = new System.ComponentModel.Container();
+
+            this.s = new NetSpell.SpellChecker.Spelling(this.components);
+
+            this.s.MisspelledWord +=
+                new NetSpell.SpellChecker.Spelling.MisspelledWordEventHandler(
+                                            this.spelling_Mispelled);
+
             folderBrowserDialog1.ShowDialog();
 
             textBox1.Text = folderBrowserDialog1.SelectedPath;
@@ -52,12 +51,66 @@ namespace BulkSpell
             directory = new System.IO.DirectoryInfo(textBox1.Text);
             filesList = directory.GetFiles();
 
+            string text;
             int i = 1;
             foreach (System.IO.FileInfo file in filesList)
             {
-                dt.Rows.Add(i, file.Name, "errors detected");
+                currentfilename = file.Name;
+
+                using (var streamReader = new System.IO.StreamReader(file.FullName, Encoding.UTF8))
+                {
+                    text = streamReader.ReadToEnd();
+                }
+                s.Text = text;
+                s.ShowDialog = false;
+
+                mispellings = 0;
+                while (s.WordIndex + 1 < s.WordCount)
+                {
+                    s.SpellCheck(s.WordIndex + 1);
+                }
+                
+
+                dt.Rows.Add(i, file.Name, mispellings.ToString());
+                i++;
             }
             dataGridView1.DataSource = dt;
+            label2.Text = WrongWords.Count.ToString();
+
+            if (WrongWords.Count > 0 )
+            {
+                label2.ForeColor = Color.Red;
+            }
+            else
+            {
+                label2.ForeColor = Color.Green;
+            }
         }
+
+        private void spelling_Mispelled(object sender, NetSpell.SpellChecker.SpellingEventArgs e)
+        {
+            List<string> tempFilesList = new List<string>();
+
+            if (WrongWords.ContainsKey(s.CurrentWord))
+            {
+                tempFilesList = WrongWords[s.CurrentWord];
+                WrongWords.Remove(s.CurrentWord);
+            }      
+
+            if (!tempFilesList.Contains(currentfilename))
+            {
+                tempFilesList.Add(currentfilename);
+            }
+            
+            WrongWords.Add(s.CurrentWord, tempFilesList);
+            mispellings += 1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ReverseErrorsForm r = new ReverseErrorsForm(WrongWords);
+            r.Show();
+        }
+
     }
 }
