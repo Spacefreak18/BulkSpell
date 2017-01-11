d4cedfc795d49775f4478218742cb1105886b791
Paul Jones <pjones@corp.ipipenet.com>
2017-01-11 13:02:29 -0500

    Refactored main bulk spell checking routine into its own class.

---
+++ src/BulkSpell.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using NetSpell.SpellChecker;
+
+namespace BulkSpell
+{
+    public class BulkSpell
+    {
+        public Dictionary<string, List<string>> Misspellings;
+        public DataTable WrongWords;
+
+        private NetSpell.SpellChecker.Dictionary.WordDictionary WordDictionary;
+        private Spelling spellchecker;  
+
+        // needed when the mispelled word event fires
+        private int MisspellingsCount;
+        private string currentfilename;
+
+        public BulkSpell(string DictionaryFile)
+        {
+            WordDictionary = new NetSpell.SpellChecker.Dictionary.WordDictionary();
+            WordDictionary.DictionaryFile = DictionaryFile;
+            WordDictionary.Initialize();
+        }
+
+        public void SpellCheck(string FolderWithFilesToBeChecked)
+        {
+            spellchecker = new NetSpell.SpellChecker.Spelling();
+            spellchecker.Dictionary = WordDictionary;
+            spellchecker.MisspelledWord += new NetSpell.SpellChecker.Spelling.MisspelledWordEventHandler(this.WordMispelled);
+
+            Misspellings = new Dictionary<string, List<string>>();
+            WrongWords = new DataTable();
+            WrongWords.Columns.AddRange(new DataColumn[3] { new DataColumn("Id"), new DataColumn("Name"), new DataColumn("Errors") });
+
+            // check if files exist or are in use
+            System.IO.FileInfo[] filesList = null;
+            System.IO.DirectoryInfo directory;
+            directory = new System.IO.DirectoryInfo(FolderWithFilesToBeChecked);
+            filesList = directory.GetFiles();
+
+            string text;
+            int i = 1;
+            foreach (System.IO.FileInfo file in filesList)
+            {
+                currentfilename = file.Name;
+
+                string mime;
+                byte[] buffer = new byte[256];
+                using (System.IO.FileStream fs = new System.IO.FileStream(file.FullName, System.IO.FileMode.Open))
+                {
+                    if (fs.Length >= 256)
+                        fs.Read(buffer, 0, 256);
+                    else
+                        fs.Read(buffer, 0, (int)fs.Length);
+                }
+
+                mime = FileTypeCheck.GetMimeType(buffer, file.FullName);
+                if (mime == "application/pdf")
+                {
+                    text = Util.ConvertPdftoText(file.FullName);
+                }
+                else
+                {
+                    if (mime == "application/octet-stream")
+                    {
+                        bool isbinary = FileTypeCheck.IsBinary(buffer);
+
+                        if (isbinary == false)
+                        {
+                            using (var streamReader = new System.IO.StreamReader(file.FullName, Encoding.UTF8))
+                            {
+                                text = streamReader.ReadToEnd();
+                            }
+                        }
+                        else
+                            text = string.Empty;
+                    }
+                    else
+                        text = string.Empty;
+                }
+
+
+                spellchecker.Text = text;
+                spellchecker.ShowDialog = false;
+
+                MisspellingsCount = 0;
+                spellchecker.WordIndex = -1;
+                while (spellchecker.WordIndex < spellchecker.WordCount - 1)
+                {
+                    spellchecker.SpellCheck(spellchecker.WordIndex + 1);
+                    spellchecker.WordIndex = spellchecker.WordIndex + 1;
+                }
+
+
+                WrongWords.Rows.Add(i, file.Name, MisspellingsCount.ToString());
+                i++;
+            }
+        }
+
+        private void WordMispelled(object sender, NetSpell.SpellChecker.SpellingEventArgs e)
+        {
+            List<string> tempFilesList = new List<string>();
+
+            if (Misspellings.ContainsKey(spellchecker.CurrentWord))
+            {
+                tempFilesList = Misspellings[spellchecker.CurrentWord];
+                Misspellings.Remove(spellchecker.CurrentWord);
+            }
+
+            if (!tempFilesList.Contains(currentfilename))
+            {
+                tempFilesList.Add(currentfilename);
+            }
+
+            Misspellings.Add(spellchecker.CurrentWord, tempFilesList);
+            MisspellingsCount += 1;
+        }
+    }
+}
--- src/MainForm.cs
+++ src/MainForm.cs
@@ -7,20 +7,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using NetSpell.SpellChecker;
-using iTextSharp.text.pdf;
-using iTextSharp.text.pdf.parser;
 
 namespace BulkSpell
 {
     public partial class MainForm : Form
     {
-        private NetSpell.SpellChecker.Dictionary.WordDictionary dict;
-        private Spelling s;
-        private int mispellings;
-        private Dictionary<string, List<string>> WrongWords = new Dictionary<string,List<string>>();
-        DataTable dt;
-        private string currentfilename;
+        private BulkSpell BulkSpellChecker;
         private string DictionaryPath = null;
 
         public MainForm()
@@ -35,136 +27,20 @@ namespace BulkSpell
 
         private void button1_Click(object sender, EventArgs e)
         {
-            currentfilename = string.Empty;
-            mispellings = 0;
-            WrongWords.Clear();
-
-            this.components = new System.ComponentModel.Container();
-
-            this.s = new NetSpell.SpellChecker.Spelling(this.components);
-            
-            s.Dictionary = dict;
-
-            this.s.MisspelledWord += new NetSpell.SpellChecker.Spelling.MisspelledWordEventHandler(this.spelling_Mispelled);
-
-            folderBrowserDialog1.ShowDialog();
-
-            textBox1.Text = folderBrowserDialog1.SelectedPath;
-
-            dt = new DataTable();
-            dt.Columns.AddRange(new DataColumn[3] { new DataColumn("Id"), new DataColumn("Name"), new DataColumn("Errors") });
-
-            // check if files exist or are in use
-            System.IO.FileInfo[] filesList = null;
-            System.IO.DirectoryInfo directory;
-            directory = new System.IO.DirectoryInfo(textBox1.Text);
-            filesList = directory.GetFiles();
-
-            string text;
-            int i = 1;
-            foreach (System.IO.FileInfo file in filesList)
+            try
             {
-                currentfilename = file.Name;
-
-                // should use a more reliable way to detect file type
-                string mime;
-                byte[] buffer = new byte[256];
-                using (System.IO.FileStream fs = new System.IO.FileStream(file.FullName, System.IO.FileMode.Open))
-                {
-                    if (fs.Length >= 256)
-                        fs.Read(buffer, 0, 256);
-                    else
-                        fs.Read(buffer, 0, (int)fs.Length);
-                }
-
-                mime = FileTypeCheck.GetMimeType(buffer, file.FullName);
-                if (mime == "application/pdf")
-                {
-                    StringBuilder pdftext = new StringBuilder();
-                    PdfReader pdfReader = new PdfReader(file.FullName);
-
-                    for (int page = 1; page <= pdfReader.NumberOfPages; page++)
-                    {
-                        ITextExtractionStrategy strategy = new SimpleTextExtractionStrategy();
-                        string currentText = PdfTextExtractor.GetTextFromPage(pdfReader, page, strategy);
-
-                        currentText = Encoding.UTF8.GetString(ASCIIEncoding.Convert(Encoding.Default, Encoding.UTF8, Encoding.Default.GetBytes(currentText)));
-                        pdftext.Append(currentText);
-                    }
-                    pdfReader.Close();
-                    text = pdftext.ToString();
-                }
-                else
-                {
-                    if (mime == "application/octet-stream")
-                    {
-                        bool isbinary = FileTypeCheck.IsBinary(buffer);
-
-                        if (isbinary == false)
-                        {
-                            using (var streamReader = new System.IO.StreamReader(file.FullName, Encoding.UTF8))
-                            {
-                                text = streamReader.ReadToEnd();
-                            }
-                        }
-                        else
-                            text = string.Empty;
-                    }
-                    else
-                        text = string.Empty;               
-                }
-                
-                
-                s.Text = text;
-                s.ShowDialog = false;
-
-                mispellings = 0;
-                s.WordIndex = -1;
-                while (s.WordIndex < s.WordCount - 1)
-                {
-                    s.SpellCheck(s.WordIndex + 1);
-                    s.WordIndex = s.WordIndex + 1;
-                }
-                
-
-                dt.Rows.Add(i, file.Name, mispellings.ToString());
-                i++;
+                runBulkSpellCheck();
             }
-            dataGridView1.DataSource = dt;
-            label2.Text = WrongWords.Count.ToString();
-
-            if (WrongWords.Count > 0 )
+            catch
             {
-                label2.ForeColor = Color.Red;
-            }
-            else
-            {
-                label2.ForeColor = Color.Green;
-            }
-        }
 
-        private void spelling_Mispelled(object sender, NetSpell.SpellChecker.SpellingEventArgs e)
-        {
-            List<string> tempFilesList = new List<string>();
-
-            if (WrongWords.ContainsKey(s.CurrentWord))
-            {
-                tempFilesList = WrongWords[s.CurrentWord];
-                WrongWords.Remove(s.CurrentWord);
-            }      
-
-            if (!tempFilesList.Contains(currentfilename))
-            {
-                tempFilesList.Add(currentfilename);
             }
             
-            WrongWords.Add(s.CurrentWord, tempFilesList);
-            mispellings += 1;
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            ReverseErrorsForm r = new ReverseErrorsForm(WrongWords);
+            ReverseErrorsForm r = new ReverseErrorsForm(BulkSpellChecker.Misspellings);
             r.Show();
         }
 
@@ -174,16 +50,33 @@ namespace BulkSpell
             openFileDialog1.ShowDialog();
             textBox2.Text = openFileDialog1.FileName;
             DictionaryPath = textBox2.Text;
-
-            dict = new NetSpell.SpellChecker.Dictionary.WordDictionary();
-            dict.DictionaryFile = DictionaryPath;
-            dict.Initialize();
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
             saveFileDialog1.ShowDialog();
-            Util.SaveDataTableToPipeDelimitedTextFile(saveFileDialog1.FileName, dt);
+            Util.SaveDataTableToPipeDelimitedTextFile(saveFileDialog1.FileName, BulkSpellChecker.WrongWords);
+        }
+
+        private void runBulkSpellCheck()
+        {
+            folderBrowserDialog1.ShowDialog();
+            textBox1.Text = folderBrowserDialog1.SelectedPath;
+
+            BulkSpellChecker = new BulkSpell(DictionaryPath);
+            BulkSpellChecker.SpellCheck(textBox1.Text);
+
+            dataGridView1.DataSource = BulkSpellChecker.WrongWords;
+            label2.Text = BulkSpellChecker.Misspellings.Count.ToString();
+
+            if (BulkSpellChecker.Misspellings.Count > 0)
+            {
+                label2.ForeColor = Color.Red;
+            }
+            else
+            {
+                label2.ForeColor = Color.Green;
+            }
         }
 
     }
